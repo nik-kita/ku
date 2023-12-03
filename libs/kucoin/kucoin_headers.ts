504aa9da8164418303184d9d4feb7dd613dfd987
nikita <nik.moiseienko@gmail.com>
2023-12-04 00:48:28 +0100

    deno init, kucoin_headers.ts

---
+++ libs/kucoin/kucoin_headers.ts
@@ -0,0 +1,102 @@
+import { crypto } from "https://deno.land/std@0.208.0/crypto/mod.ts";
+import { encodeBase64 } from "https://deno.land/std@0.208.0/encoding/base64.ts";
+
+const te = new TextEncoder();
+
+function stringify({
+  endpoint,
+  method,
+  timestamp,
+  body,
+}: {
+  endpoint: string;
+  timestamp: number;
+  method: "POST" | "GET" | "DELETE";
+  body?: object | string;
+}) {
+  let _body: string | object = body ?? "";
+
+  if (Object(body) === body) {
+    _body = JSON.stringify(body);
+  }
+
+  return `${timestamp}${method}${endpoint}${_body}`;
+}
+
+async function sign({
+  apiSecret,
+  payload,
+}: {
+  payload: string;
+  apiSecret: string;
+}): Promise<string> {
+  const tePayload = te.encode(payload);
+  const cryptoKey = await crypto
+    .subtle
+    .importKey(
+      "raw",
+      te.encode(apiSecret),
+      { name: "HMAC", hash: "SHA-256" },
+      false,
+      ["sign"],
+    );
+  const result = encodeBase64(
+    await crypto.subtle.sign("HMAC", cryptoKey, tePayload),
+  );
+
+  return result;
+}
+
+export async function kucoin_headers(
+  {
+    method,
+    endpoint,
+    query,
+    body,
+  }: {
+    method: "POST" | "GET" | "DELETE";
+    endpoint: string;
+    query?: Record<string, unknown>;
+    body?: Record<string, unknown>;
+  },
+  {
+    key,
+    secret,
+    passphrase,
+  }: {
+    key: string;
+    secret: string;
+    passphrase: string;
+  },
+) {
+  const _endpoint = query && Object.keys(query).length
+    ? `${endpoint}?${new URLSearchParams(query as Record<string, string>)}`
+    : endpoint;
+  const timestamp = Date.now();
+  const stringToSign = stringify({
+    endpoint: _endpoint,
+    timestamp,
+    method,
+    body,
+  });
+  const [signature, signedPassphrase] = await Promise.all([
+    sign({
+      payload: stringToSign,
+      apiSecret: secret,
+    }),
+    sign({
+      payload: passphrase,
+      apiSecret: secret,
+    }),
+  ]);
+  const headers = {
+    "KC-API-SIGN": signature,
+    "KC-API-TIMESTAMP": timestamp.toString(),
+    "KC-API-KEY": key,
+    "KC-API-PASSPHRASE": signedPassphrase,
+    "KC-API-KEY-VERSION": "2",
+    "Content-Type": "application/json",
+  } as const;
+
+  return headers;
+}
