475ab938c6d3e283fc5cb9a5cce9fd50b969edfa
nikita <nik.moiseienko@gmail.com>
2023-12-25 21:39:22 +0100

    add params to accounts request

--- cli/accounts.ts
+++ cli/accounts.ts
@@ -3,6 +3,19 @@ import { Credentials } from "../libs/kucoin/kucoin_headers.ts";
 import { accounts } from "../libs/kucoin/http/accounts.ts";
 
 const credentials = await load() as Credentials;
-const res = await accounts(credentials);
+const [usdt_res, btc_res] = await Promise.all([
+  accounts(credentials, {
+    currency: "USDT",
+    type: "trade_hf",
+  }),
+  accounts(credentials, {
+    currency: "BTC",
+    type: "trade_hf",
+  }),
+]);
 
-console.log(res);
+const banner = "=".repeat(20);
+console.log(banner, "BTC", banner);
+console.log(btc_res);
+console.log(banner, "USDT", banner);
+console.log(usdt_res);
--- libs/kucoin/http/accounts.ts
+++ libs/kucoin/http/accounts.ts
@@ -4,9 +4,24 @@ import { Credentials, kucoin_headers } from "../kucoin_headers.ts";
  * @description
  * https://www.kucoin.com/docs/rest/account/basic-info/get-account-list-spot-margin-trade_hf
  */
-export async function accounts(credentials: Credentials) {
-  const headers = await kucoin_headers({ endpoint, method }, credentials);
-  const res = await fetch(host + endpoint, { headers });
+export async function accounts(
+  credentials: Credentials,
+  params?: Partial<{
+    currency: string;
+    type: "main" | "margin" | "trade" | "trade_hf";
+  }>,
+) {
+  const headers = await kucoin_headers({
+    endpoint,
+    method,
+    ...(params && { query: params }),
+  }, credentials);
+  const res = await fetch(
+    `${host}${endpoint}${
+      params ? "?" + new URLSearchParams(params).toString() : ""
+    }`,
+    { headers },
+  );
   const json = await res.json();
 
   return json as AccountListResponse;
