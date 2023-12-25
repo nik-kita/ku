bc5af77144f0683227e33ecfa288b3e291ef1796
nikita <nik.moiseienko@gmail.com>
2023-12-25 22:05:31 +0100

    cli transfers

---
+++ cli/transfer/btc_hf_trade.ts
@@ -0,0 +1,20 @@
+import { load } from "https://deno.land/std@0.210.0/dotenv/mod.ts";
+import { Credentials } from "../../libs/kucoin/kucoin_headers.ts";
+import { inner_transfer } from "../../libs/kucoin/http/inner_transfer.ts";
+import { monotonicFactory } from "https://deno.land/x/ulid@v0.3.0/mod.ts";
+
+const ulid = monotonicFactory();
+const credentials = await load() as Credentials;
+const arg = Number.parseFloat(Deno.args.at(-1)!);
+const amount = Number.isNaN(arg) ? 0.00001 : arg;
+const body = {
+  amount: amount.toString(),
+  currency: "BTC",
+  from: "trade_hf",
+  to: "trade",
+  clientOid: ulid(),
+} as const;
+console.log("body", body);
+const res = await inner_transfer(body, credentials);
+
+console.log(res);
