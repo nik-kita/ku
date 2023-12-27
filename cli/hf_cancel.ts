bef2e9d106de8fb66eab3f2b5d1673006daa8a68
nikita <nik.moiseienko@gmail.com>
2023-12-27 16:22:27 +0100

    cancel all

---
+++ cli/hf_cancel.ts
@@ -0,0 +1,7 @@
+import { load } from "https://deno.land/std@0.210.0/dotenv/mod.ts";
+import { Credentials } from "../libs/kucoin/kucoin_headers.ts";
+import { hf_cancel_by_symbol } from "../libs/kucoin/http/hf_cancel_by_symbol.ts";
+
+const credentials = await load() as Credentials;
+const res = await hf_cancel_by_symbol('BTC-USDT', credentials);
+console.log(res);
